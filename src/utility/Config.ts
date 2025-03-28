517d23afabf1bf0397ebb17ed0a048b747e5cc57
Aldin Čamdžić <acamdzic0@proton.me>
2025-03-28 01:56:55 +0100

    feat: streamline config validation and merging logic

--- src/utility/Config.ts
+++ src/utility/Config.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import { merge } from "lodash";
 import type { ZodSchema } from "zod";
 
 export class Config<T> {
@@ -7,78 +8,41 @@ export class Config<T> {
   private zodSchema: ZodSchema<T>;
   private defaults: T;
 
-  private configData: T;
+  private data: T;
 
   constructor(filePath: string, zodSchema: ZodSchema<T>, defaults: T) {
     this.filePath = filePath;
     this.zodSchema = zodSchema;
     this.defaults = defaults;
 
-    this.ensureConfigFileExists();
-
-    const rawData = fs.readFileSync(this.filePath, "utf-8");
-    const parsedData = JSON.parse(rawData);
-
-    this.configData = this.zodSchema.parse(parsedData);
+    this.validateEverything();
   }
 
-  private ensureConfigFileExists() {
+  private validateEverything() {
     const fileDir = path.dirname(this.filePath);
 
     if (!fs.existsSync(fileDir)) {
       fs.mkdirSync(fileDir, { recursive: true });
     }
 
-    const defaultConfig = this.defaults;
-
     if (!fs.existsSync(this.filePath)) {
-      fs.writeFileSync(this.filePath, JSON.stringify(defaultConfig, null, 2));
+      this.data = this.defaults;
+
+      fs.writeFileSync(this.filePath, JSON.stringify(this.defaults, null, 2));
     } else {
       const rawData = fs.readFileSync(this.filePath, "utf-8");
       const parsedData = JSON.parse(rawData);
 
-      const mergedData = {
-        ...defaultConfig,
-        ...parsedData
-      };
+      const mergedConfig = merge(this.defaults, parsedData);
+      const validatedConfig = this.zodSchema.parse(mergedConfig);
 
-      for (const key in defaultConfig) {
-        if (!(key in parsedData)) {
-          mergedData[key] = defaultConfig[key];
-        }
-      }
+      this.data = validatedConfig;
 
-      for (const key in mergedData) {
-        if (
-          Array.isArray(mergedData[key]) &&
-          Array.isArray(defaultConfig[key])
-        ) {
-          if (!mergedData[key].length) {
-            mergedData[key] = defaultConfig[key];
-          } else {
-            mergedData[key] = mergedData[key].map(item => {
-              if (item && typeof item === "object") {
-                const [defaultItem] = defaultConfig[key];
-
-                return { ...defaultItem, ...item };
-              }
-
-              return item;
-            });
-          }
-        }
-      }
-
-      const validationResult = this.zodSchema.parse(mergedData);
-
-      fs.writeFileSync(
-        this.filePath,
-        JSON.stringify(validationResult, null, 2)
-      );
+      fs.writeFileSync(this.filePath, JSON.stringify(validatedConfig, null, 2));
     }
   }
 
   get<TK extends keyof T>(key: TK): T[TK] {
-    return this.configData[key];
+    return this.data[key];
   }
 }
